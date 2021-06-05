c6a27fa361ea46a7b6fb773fa46518b302994782
Wesley Oliveira <wesleyholiveir@gmail.com>
2021-06-05 13:41:54 -0300

    Added a error handling when mysql connection times out

--- pages/chat/index.vue
+++ pages/chat/index.vue
@@ -213,8 +213,6 @@ export default {
 
   methods: {
     generateRNG(min, max) {
-      min = Math.ceil(min)
-      max = Math.floor(max)
       return Math.floor(Math.random() * (max - min + 1)) + min
     },
 
--- server/config/mysql.js
+++ server/config/mysql.js
@@ -1,6 +1,6 @@
 import { createConnection } from 'mysql2'
 
-const mysqlCon = createConnection({
+let mysqlCon = createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || '',
@@ -10,6 +10,26 @@ const mysqlCon = createConnection({
 mysqlCon.connect(err => {
   if (err) throw err;
   console.log('Conexão com o banco de dados estabelecida')
+})
+
+mysqlCon.on('error', (err) => {
+  if (err) {
+    console.log('Ocorreu um erro com o banco de dados')
+    console.error(err)
+    mysqlCon.end((e) => {
+      if (e) {
+        console.log('Ocorreu um erro ao FINALIZAR a conexão do banco de dados')
+        console.error(e)
+      }
+
+      mysqlCon = createConnection({
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USERNAME || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME || 'cebolaoquest'
+      })
+    })
+  }
 })
 
 export default mysqlCon
